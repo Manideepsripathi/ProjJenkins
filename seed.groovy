ad0f249d212f7f4de981975ba5df27bef149244d
Manideepsripathi <manideepsripathi@gmail.com>
2020-09-20 21:42:26 -0400

    Create seed.groovy
    Update the file

---
+++ seed.groovy
@@ -0,0 +1,18 @@
+  
+base_path = "Learnings"
+job_path = "${base_path}/Project"
+folder("${base_path}")
+pipelineJob(job_path) {
+    description("This job creates new pipeline")
+    logRotator {
+        daysToKeep(30)
+    }
+    
+    
+    definition {
+        cps {
+            script(readFileFromWorkspace("${job_path}/Jenkinsfile"))
+            sandbox(false)
+        }
+    }
+}
